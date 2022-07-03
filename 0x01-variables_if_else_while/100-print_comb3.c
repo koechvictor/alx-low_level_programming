58a7463a0b6453b6e413b9732fd92d67e3391a8a
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-03 12:31:27 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int nums;
+int nums1;
+
+for(nums='0'; nums<='9'; nums++)
+{
+for(numbs1='0'; nums<='9'; nums1++)
+{
+putchar(nums);
+putchar(nums1);
+if(nums == '9' && nums1 == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('/n');
+return (0);
+}
