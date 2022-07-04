0ed2885ef97218590db38eea5c8dd8e030e86fba
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-04 00:24:54 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 int nums;
 int nums1;
 
-for(nums = '0'; nums <= '9'; nums++)
+for (nums = '0'; nums <= '9'; nums++)
 {
-for(nums1 = '0'; nums <= '9'; nums1++)
+for (nums1 = '0'; nums <= '9'; nums1++)
 {
 putchar(nums);
 putchar(nums1);
-if(nums == '9' && nums1 == '9')
+if (nums == '9' && nums1 == '9')
 continue;
 putchar(',');
 putchar(' ');
