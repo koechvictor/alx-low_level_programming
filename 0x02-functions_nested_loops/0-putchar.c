080a09bea66e21346f7443f0c9037c16d5bbca97
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-05 04:18:00 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+
+#include "_putchar"
+
+/**
+ * main -Entry - point 
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+return (0);
+}
