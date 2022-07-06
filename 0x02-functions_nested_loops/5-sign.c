8b8968250612d6f2a3a69241ec33a3d67c73244e
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-06 12:46:59 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,21 @@
 
 int print_sign(int n)
 {
-	int i;
-
-	if (n > 0)
-	{
-	_putchar('+');
-	i = 1;
-	}
-	else if (n < 0)
-	i = -1;
-	}
-	else
-	{
-	_putchar('-');
-	i = 0;
-	}
-
-	return (i);
+int i;
+if (n > 0)
+{
+_putchar('+');
+i = 1;
+}
+else if (n < 0)
+{
+_putchar('-');
+i = -1;
+}
+else
+{
+_putchar('0');
+i = 0;
+}
+return (i);
 }
