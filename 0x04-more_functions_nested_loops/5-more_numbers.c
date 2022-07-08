2122dfc767d984ffa0f3e8111c326a7670e8f491
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-08 08:59:47 -0700

    task10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,12 @@ void more_numbers(void);
 	int i;
 	int x;
 
-	for (x = 0; x < 10; x++)
-	{
-		for (i = 0; i <= 14; i++)
+	for (x = '0'; x < 10; x++)
+		for (i = '0'; i <= 14; i++)
 	{
 		if (i > 9)
 		_putchar((i / 10) + '0');
 		_putchar((i % 10) + '0');
 	}
 		_putchar('\n');
-	}
 }
