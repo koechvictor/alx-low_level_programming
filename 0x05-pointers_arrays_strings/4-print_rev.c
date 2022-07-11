9ce00af1a1ba3cd8ce19856999d39ee55a49f8b5
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-11 06:29:09 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string
+ * Return: string length
+ */
+void print_rev(char *s)
+{
+	int one, two, three;
+	char rev;
+	 
+	one = 0;
+
+	while (s[one] != '\0')
+		one++;
+
+	one--;
+	for (two = 0; two <= one / 2; two++)
+	{
+		rev = s(one);
+		three = s(one - two);
+		s(two) = three;
+		s(one-two) = rev;
+	}
+}
