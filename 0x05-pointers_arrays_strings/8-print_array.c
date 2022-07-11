2816f73638b827da31de72ae3370b0eec6ec04d4
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-11 10:46:31 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array.
+ * @a: variable 1 to be to be printed.
+ * @n: variable 2 to be printed.
+ */
+void print_array(int *a, int n);
+{
+	int one;
+
+	one = 0;
+
+	while (one < n)
+	{
+		if (one != (n - 1))
+			printf("%i, ", a[one]);
+		else
+			printf("%i", a[one]);
+		one++;
+	}
+	printf("\n");
+}
