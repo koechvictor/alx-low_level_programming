855cbe7e69710d71cb267e5d446f59e1187f3aa3
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-13 12:39:23 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: first string to compare
+ * @s2: second string to compare
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int luck = 0;
+
+	while (*(s1 + luck) != '\0' || *(s2 + luck) != '\0')
+	{
+		if (*(s1 + luck) != *(s2 + luck))
+			return (*(s1 + luck) - *(s2 + luck));
+		luck++;
+	}
+	return (0);
+}
+
+
