833a3a04c4e1b6d7f65ee2247b44666058bb78a1
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-13 10:53:32 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string.
+ * @dest: first string to copy
+ * @src: second string to copy
+ * @n: number of bites to use from src
+ * Return: concatenated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *luck = dest;
+
+	for (;n != 0 && *src != 0; n--, src++)
+	{
+		*luck = *src;
+		luck++;
+	}
+	for (;n != 0 && *src != '\0'; n--, luck++)
+	luck = '\0';
+	return (dest);
+}
