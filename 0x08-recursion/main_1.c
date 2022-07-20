763f91bae43c6b4283e9d0ff3c1e3a0e268a5c73
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-20 08:20:49 -0700

    update3

---
+++ 0x08-recursion/main_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = factorial(7);
+	printf("%d\n", r);
+	return (0);
+}
