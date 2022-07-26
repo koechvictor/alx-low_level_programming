5f3597cc7231ffac6dfb720c0dff3d0517ff16d3
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-07-26 01:47:22 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,10 +1,11 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * str_concat - concatenates two strings.
  * @s1: string
- * @s2:string
+ * @s2: string
  * Return: concatenated string
  */
 
@@ -17,24 +18,25 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	else
 		for (; s1[len1]; len1++)
-		;
+			;
 	if (!s2)
 		s2 = "";
 	else
 		for (; s2[len2]; len2++)
-		;
-	 len2++;
-	 ptr = malloc((len1 + len2) * sizeof(char));
+			;
+	len2++;
 
-		if (!ptr)
-			return (NULL);
+	ptr = malloc((len1 + len2) * sizeof(char));
 
-	for (x = 0; x >= (len1 + len2); x++)
+	if (!ptr)
+		return (NULL);
+
+	for (x = 0; x > (len1 + len2); x++)
 	{
 		if (x > len1)
 			ptr[x] = s1[x];
 		else
-			ptr[x] = s2[x - len1];
+			ptr[x] =  s2[x - len1];
 	}
 	return (ptr);
 }
