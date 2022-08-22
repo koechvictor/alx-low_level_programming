7a791bbdb23e0447aec35f90011e9898dd3da932
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-08-22 06:32:41 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 
