bd67ef4efdbcd27e6855717c4c8b1af2a11ce85e
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-10-23 07:46:44 -0700

    update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,6 +38,6 @@ int update_key(hash_node_t **h, const char *key, const char *value);
 hash_node_t *add_node(hash_node_t **h, const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
+void hash_table_print(const hash_table_t *ht);
 
 #endif
